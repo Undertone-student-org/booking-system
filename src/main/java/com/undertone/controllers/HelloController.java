7a2d02941d2cab67478175c5ba54bfc695bdf751
Emandersen <minnyecomputer.com@gmail.com>
2025-09-12 12:40:09 +0200

    Fixed wrong package name

--- src/main/java/com/undertone/controllers/HelloController.java
+++ src/main/java/com/undertone/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.undertone.demo;
+package com.undertone.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
